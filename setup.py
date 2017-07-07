2493979027d7f1533f0d585fe667801ef8e8eb03
Guillaume Roche <guillaume.roche@ftsn.me>
2017-07-07 15:45:03 +0200

    Init the repository architecture and add a .gitignore

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ambre',
+    version='0.0',
+    description='',
+    author=[
+        "Marc Souply",
+        "Guillaume Roche",
+        "Louanne Roger",
+        "Anatole Gaudin"
+    ],
+    author_email=[
+        "guillaume.roche@ftsn.me"
+        # To fill for marc, louanne and anatole
+    ],
+    packages=find_packages(),
+    install_requires=[
+        'pymongo'
+    ],
+    include_package_data=True,
+    data_files=[
+    ],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Scientific/Engineering :: Information Analysis"
+    ],
+    test_suite='tests'
+)
+print(find_packages())
